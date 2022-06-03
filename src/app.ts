bbfa5bb3ff1cc6f7f996fd1c1f6f8eddd2208d38
julio cesar <jc10275@outlook.com>
2022-06-02 23:16:41 -0300

    setup api documentation

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+
 const app = express();
+
+app.use('/apiDocs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   return res.send('Hello World!');
 });
 
 app.listen('5000', () => {
-  console.log('Server is on');
+  console.log('Server is on\nhttp://localhost:5000');
 });
